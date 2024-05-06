c76813eb420fc44b0bd59981aeb890df801a3f96
Shamendry <156168437+Shamendry@users.noreply.github.com>
2024-05-06 22:32:48 +0530

    Update Datascience_dashboard.py

--- Datascience_dashboard.py
+++ Datascience_dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
+import numpy as np
 
 #Loading the dataset
 df = pd.read_excel('MingerCleaned_data.xlsx')
